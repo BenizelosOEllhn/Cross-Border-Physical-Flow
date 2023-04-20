dcf6eb827012d2dafd531a5b548a0ffd17d5c187
BenizelosOEllhn <79727099+BenizelosOEllhn@users.noreply.github.com>
2023-04-21 01:52:24 +0300

    Update terry_davis.py
    προστεθηκε παιχνιδι με χωρες θα ακολουθησει σπασιμο σε συναρτησεις

--- terry_davis.py
+++ terry_davis.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import requests
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Create webdriver object and open login page
 driver = webdriver.Chrome()
@@ -23,9 +24,20 @@ username_input.send_keys(username)
 password_input.send_keys(password)
 submit_button.click()
 
-# Wait for login to complete and navigate to file download page
+
+# wait for login to complete and navigate to file download page
 driver.get(url)
-driver.get('https://transparency.entsoe.eu/transmission-domain/physicalFlow/show?name=&defaultValue=false&viewType=TABLE&areaType=BORDER_BZN&atch=false&dateTime.dateTime=19.04.2023+00:00|UTC|DAY&border.values=CTY|10YGR-HTSO-----Y!BZN_BZN|10YGR-HTSO-----Y_BZN_BZN|10YAL-KESH-----5&dateTime.timezone=UTC&dateTime.timezone_input=UTC')
+print('1:AL\t2:AT\t3:BE\t4:BA\t5:BG\n6:HR\t7:CZ\t8:DE\t9:EE\t10:FI\n11:FR\t12:GE\t13:DE\t14:GR\t15:HU\n16:IE\t17:IT\t18:XK\t19:LV\t20:LT\n21:LU\t22:MT\t23:MD\t24:ME\t25:NL\n26:MK\t27:NO\t28:PL\t29:PT\t30:RO\n31:RS\t32:SK\t33:SI\t34:ES\t35:SE\n36:CH\t37:TR\t38:UA\t39:UK')
+number = input("Give a number from 1 to 39 for the desired country")
+def get_country(number):
+    checkbox = driver.find_element(By.XPATH, '//input[@id="{}"]'.format(number))
+    parent_element = checkbox.find_element(By.XPATH, '..')
+    country = parent_element.find_element(By.XPATH, './/a[@href]')
+    print(country)
+    hover = ActionChains(driver).move_to_element(country)
+    hover.perform()
+    country.click()
+get_country(number)
 dropdown = driver.find_element('id', 'dv-export-data')
 dropdown.click()
 
