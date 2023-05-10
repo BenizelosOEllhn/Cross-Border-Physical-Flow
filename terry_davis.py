370a235cdd0d50c3db6d3383f5302645c64d71c3
Andreas Panagopoulos <a.panagopsch@gmail.com>
2023-05-10 17:19:02 +0300

    added incomplete code that can be fixed really easily

--- terry_davis.py
+++ terry_davis.py
@@ -3,18 +3,22 @@ from selenium.webdriver.common.by import By
 import requests
 from selenium.webdriver.common.action_chains import ActionChains
 from os import path
+import time
+import csv
 
 
 URL = 'https://transparency.entsoe.eu/transmission-domain/physicalFlow/show#'
 username = 'panospanopoulos5@gmail.com'
 password = 'confirmpassword!'
 
+
+
 def open_login_page(url):
     # Create webdriver object and open login page
     driver = webdriver.Chrome()
     chrome_options = webdriver.ChromeOptions()
     # Changing download directory for session to running file  path. Future iterations should include path options
-    dir_path = path.dirname(path.realpath(__file__)) 
+    dir_path = path.dirname(path.realpath(__file__))
     prefs = {'download.default_directory' : dir_path}
     chrome_options.add_experimental_option('prefs', prefs)
     driver = webdriver.Chrome(chrome_options=chrome_options)
@@ -66,6 +70,47 @@ def download_file(driver, url):
     # Wait for download link to be available and get download link URL
     element = driver.find_element(By.XPATH, "//div[@class='export-item']//a[@dataitem='ALL' and @timerange='YEAR' and @exporttype='CSV']")
     driver.execute_script("arguments[0].click();", element) #clicking normally doesn't work. asking driver to click using javascript
+def get_sum():
+    with open("Cross-Border Physical Flow_202301010000-202401010000.csv", newline='') as my_data:
+        # Create a CSV reader object
+        reader = csv.reader(my_data, delimiter=',')
+        next(reader)
+
+        # Create an empty list to store the data
+        data = [0,0]
+        all_imported_data=[0,0]
+        all_exported_data=[0,0]
+
+        # Loop over each row in the CSV file
+        for row in reader:
+            # Create a dictionary to store the data for this row
+            row_data = {}
+
+            # Extract the values from the row and store them in the dictionary
+            if  row[1] and row[1]!="N/A" and row[1]!="-":
+                row_data['imported'] = int(row[1])
+                row_data['exported'] = int(row[2])
+
+            # Add the dictionary to the list of data
+                data[0]+=row_data['imported']
+                data[1]+=row_data['exported']
+                all_imported_data.append(row_data['imported'])
+                all_exported_data(row['exported'])
+            else:
+                continue
+
+        print(f'Imported: {data[0]},Exported :{data[1]}')
+        return (data[0],data[1],all_imported_data,all_exported_data)
+def get_difference():
+    x1,x2=get_sum()
+    diff=abs(x1-x2)
+    print(diff)
+    return diff
+
+
+def get_extrema():
+    pass
+
 
 def close_driver(driver):
     # Close the driver
@@ -76,7 +121,10 @@ def main():
     driver = open_login_page(URL)
     login(driver, username, password)
     download_file(driver, URL)
+    time.sleep(20)
     close_driver(driver)
+    get_sum()
+    get_difference()
 
 
 if __name__ == '__main__':
